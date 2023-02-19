8847fee7a5091325f15f5a770e4cc44d9d3ea48b
mirroredcube <anitha.praveenrao@gmail.com>
2023-02-19 23:05:25 +0100

    Update sever url

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // Fetch data from server using OpenAI api
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codegpt-0tjx.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
